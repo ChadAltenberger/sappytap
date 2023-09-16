d40fd76b50556e7f6867b5e66491d42f496ba8f0
Chad <chad@summitathletics.com>
2023-09-16 11:19:03 -0400

    Imported assets + started layout

--- src/assets/js/components/responsive-video.js
+++ src/assets/js/components/responsive-video.js
@@ -7,17 +7,14 @@ export default function responsiveVideo() {
             const vid640 = video.getAttribute("data-vid-640");
             const vid960 = video.getAttribute("data-vid-960");
             const vid1280 = video.getAttribute("data-vid-1280");
-            const vid1920 = video.getAttribute("data-vid-1920");
             if (window.innerWidth < 426) {
                 video.setAttribute("src", vid426);
             } else if (window.innerWidth >= 426 && window.innerWidth < 640) {
                 video.setAttribute("src", vid640);
             } else if (window.innerWidth >= 640 && window.innerWidth < 960) {
                 video.setAttribute("src", vid960);
-            } else if (window.innerWidth >= 960 && window.innerWidth < 1280) {
+            } else if (window.innerWidth >= 960) {
                 video.setAttribute("src", vid1280);
-            } else if (window.innerWidth >= 1280) {
-                video.setAttribute("src", vid1920);
             }
         });
     };
--- src/assets/js/pages/index.js
+++ src/assets/js/pages/index.js
@@ -1,6 +1,7 @@
-import triggerPopup from "../components/trigger-popup.js";
+import responsiveVideo from "../components/responsive-video";
 
 export default function indexInit() {
+    responsiveVideo();
     // All main index related JavaScript goes here...
     /* ============= TRIGGER POPUP ON PAGE LOAD ============= */
     // triggerPopup("#popup");
