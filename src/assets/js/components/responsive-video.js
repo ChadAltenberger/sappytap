39588d89f662a1a69398b122ba63872508d926c3
Chad <chad@summitathletics.com>
2023-09-18 10:03:51 -0400

    Finished layout + webp usage/responsive work

--- src/assets/js/components/responsive-video.js
+++ src/assets/js/components/responsive-video.js
@@ -3,13 +3,10 @@ export default function responsiveVideo() {
 
     const setVidResponsive = () => {
         videos.forEach((video) => {
-            const vid426 = video.getAttribute("data-vid-426");
             const vid640 = video.getAttribute("data-vid-640");
             const vid960 = video.getAttribute("data-vid-960");
             const vid1280 = video.getAttribute("data-vid-1280");
-            if (window.innerWidth < 426) {
-                video.setAttribute("src", vid426);
-            } else if (window.innerWidth >= 426 && window.innerWidth < 640) {
+            if (window.innerWidth < 640) {
                 video.setAttribute("src", vid640);
             } else if (window.innerWidth >= 640 && window.innerWidth < 960) {
                 video.setAttribute("src", vid960);
